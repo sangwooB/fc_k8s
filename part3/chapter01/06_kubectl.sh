9d8e64870d07da912e75668753729d4ce8d1cd35
SangwooBae <sangwoo.bae@bespinglobal.com>
2022-09-30 15:57:12 +0900

    first commit

--- part3/chapter01/06_kubectl.sh
+++ part3/chapter01/06_kubectl.sh
@@ -14,7 +14,7 @@ kubectl explain pods.spec.containers
 kubectl get nodes
 
 # 쿠버네티스 오브젝트 생성/변경
-kubectl apply -f 01_06_deployment.yaml
+kubectl apply -f 06_deployment.yaml
 
 # 실행 중인 Pod(컨테이너) 목록 조회 
 kubectl get pods
@@ -26,7 +26,7 @@ kubectl scale -f deployment.yaml --replicas=3
 kubectl diff -f deployment.yaml
 
 # 쿠버네티스 오브젝트의 spec을 editor로 편집 (replicas를 4로 변경)
-kubectl edit deployment/nginx-deployment: 
+kubectl edit deployment nginx-deployment
 
 # 로컬 포트는 파드에서 실행 중인 컨테이너 포트로 포워딩
 kubectl port-forward pod/nginx-deployment-74bfc88f4d-fkfjc 8080:80
